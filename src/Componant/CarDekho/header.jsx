59d54b15f59315c09f704d07b1bc9c01859240b4
rakshit <rakshitnagaraj951@gmail.com>
2025-02-06 15:37:38 +0530

    first commit

--- src/Componant/CarDekho/header.jsx
+++
@@ -1,6 +0,0 @@
-
-
-
-import React from "react";
-import "./Header.css"; // Import the CSS file
-
---
+++ src/Componant/food/header.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../header";
+export default{
+    title: 'Components/Foodislice/Header',
+    component: Header,
+  };
+  
+
+export const Default=()=><header/>
