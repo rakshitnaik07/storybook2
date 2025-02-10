e043a84e5420a9c7884f71996495f1df3519a071
rakshit <rakshitnagaraj951@gmail.com>
2025-02-10 13:45:49 +0530

    first-commit

--- src/Componant/foodslice/Card.jsx
+++ src/Componant/foodslice/Card.jsx
@@ -2,102 +2,102 @@
 export const menu=[
     {
        id:1,
-       image:"./JustDail/Images/Restaurants.png",
+       image:"./justdail/Images/Restaurants.png",
        title: "Restaurants"
     },
     {
         id:2,
-        image:"./JustDail/Images/Hotels.png",
+        image:"./justdail/Images/Hotels.png",
         title: "Hotels"
      },
      {
         id:3,
-        image:"./JustDail/Images/Beauty.png",
+        image:"./justdail/Images/Beauty.png",
         title: "Beauty"
      },
      {
         id:4,
-        image:"./JustDail/Images/Home.png",
+        image:"./justdail/Images/Home.png",
         title: "Home Decor"
      },
      {
         id:5,
-        image:"./JustDail/Images/Wedding.png",
+        image:"./justdail/Images/Wedding.png",
         title: "Wedding Planning"
      },
      {
          id:6,
-         image:"./JustDail/Images/Education.png",
+         image:"./justdail/Images/Education.png",
          title: "Education"
       },
       {
          id:7,
-         image:"./JustDail/Images/Rent.png",
+         image:"./justdail/Images/Rent.png",
          title: "Rent & Hire"
       },
       {
          id:8,
-         image:"./JustDail/Images/Hospitals.png",
+         image:"./justdail/Images/Hospitals.png",
          title: "Hospitals"
       },
       {
         id:9,
-        image:"./JustDail/Images/Contractors.png",
+        image:"./justdail/Images/Contractors.png",
         title: "Contractors"
      },
      {
          id:10,
-         image:"./JustDail/Images/Pet.png",
+         image:"./justdail/Images/Pet.png",
          title: "Pet Shops"
       },
       {
          id:11,
-         image:"./JustDail/Images/Restaurants.png",
+         image:"./justdail/Images/Restaurants.png",
          title: "Restaurants"
       },
       {
           id:12,
-          image:"./JustDail/Images/Hotels.png",
+          image:"./justdail/Images/Hotels.png",
           title: "Hotels"
        },
        {
           id:13,
-          image:"./JustDail/Images/Beauty.png",
+          image:"./justdail/Images/Beauty.png",
           title: "Beauty"
        },
        {
           id:14,
-          image:"./JustDail/Images/Home.png",
+          image:"./justdail/Images/Home.png",
           title: "Home Decor"
        },
        {
           id:15,
-          image:"./JustDail/Images/Wedding.png",
+          image:"./justdail/Images/Wedding.png",
           title: "Wedding Planning"
        },
        {
            id:16,
-           image:"./JustDail/Images/Education.png",
+           image:"./justdail/Images/Education.png",
            title: "Education"
         },
         {
            id:17,
-           image:"./JustDail/Images/Rent.png",
+           image:"./justdail/Images/Rent.png",
            title: "Rent & Hire"
         },
         {
            id:18,
-           image:"./JustDail/Images/Hospitals.png",
+           image:"./justdail/Images/Hospitals.png",
            title: "Hospitals"
         },
         {
           id:19,
-          image:"./JustDail/Images/Contractors.png",
+          image:"./justdail/Images/Contractors.png",
           title: "Contractors"
        },
        {
            id:20,
-           image:"./JustDail/Images/Pet.png",
+           image:"./justdail/Images/Pet.png",
            title: "Pet Shops"
         },
 
